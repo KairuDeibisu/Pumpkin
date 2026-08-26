544708fe7747fc51518ec0d8650fb468fad6c2c2
Kyle Davis <kyled@kyledavis.dev>
2026-08-26 12:21:56 -0400

    feat(gametest): add block entity and test block world hooks

--- crates/pumpkin-gametest/src/world.rs
+++ crates/pumpkin-gametest/src/world.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use pumpkin_data::BlockStateId;
+use pumpkin_nbt::NbtCompound;
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_world::world::BlockFlags;
 
@@ -15,6 +16,22 @@ pub trait GameTestWorld: Send + Sync {
         block_state_id: BlockStateId,
         flags: BlockFlags,
     ) -> GameTestResult<()>;
+
+    async fn set_block_entity_nbt(
+        &self,
+        position: &BlockPos,
+        nbt: &NbtCompound,
+    ) -> GameTestResult<()>;
+
+    async fn update_test_block_redstone(&self, position: &BlockPos) -> GameTestResult<()>;
+
+    async fn trigger_test_block(&self, position: &BlockPos) -> GameTestResult<()>;
+
+    async fn reset_test_block(&self, position: &BlockPos) -> GameTestResult<()>;
+
+    async fn test_block_triggered(&self, position: &BlockPos) -> GameTestResult<bool>;
+
+    async fn test_block_message(&self, position: &BlockPos) -> GameTestResult<String>;
 
     async fn surface_height(&self, x: i32, z: i32) -> i32;
 }
