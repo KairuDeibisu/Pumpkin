7504ece43fe4f951ac5fe14b9a5d159b5a83ec80
Kyle Davis <kyled@kyledavis.dev>
2026-08-26 13:51:21 -0400

    revert(gametest): remove incremental placement export

--- crates/pumpkin-gametest/src/structure/mod.rs
+++ crates/pumpkin-gametest/src/structure/mod.rs
@@ -1,7 +1,5 @@
 mod placement;
 mod template;
 
-pub use placement::{
-    PlacedStructure, StructurePlacement, clear_structure_area, place_structure,
-};
+pub use placement::{PlacedStructure, clear_structure_area, place_structure};
 pub use template::{StructureBlock, StructureTemplate, TestBlockMode};
