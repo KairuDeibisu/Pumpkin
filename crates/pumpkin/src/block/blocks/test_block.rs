612096541e3450a833b42596cafb315f8210b127
Kyle Davis <kyled@kyledavis.dev>
2026-09-03 12:22:17 -0400

    fix(gametest): expose test block start pulse

--- crates/pumpkin/src/block/blocks/test_block.rs
+++ crates/pumpkin/src/block/blocks/test_block.rs
@@ -6,7 +6,10 @@ use pumpkin_util::{GameMode, PermissionLvl};
 use crate::block::entities::test_block::TestBlockBlockEntity;
 use crate::block::entities::test_instance_block::TestInstanceBlockBlockEntity;
 use crate::block::registry::BlockActionResult;
-use crate::block::{BlockBehaviour, NormalUseArgs, PlacedArgs};
+use crate::block::{
+    BlockBehaviour, EmitsRedstonePowerArgs, GetRedstonePowerArgs, NormalUseArgs,
+    OnScheduledTickArgs, PlacedArgs,
+};
 
 #[pumpkin_block("minecraft:test_block")]
 pub struct TestBlock;
@@ -29,6 +32,34 @@ impl BlockBehaviour for TestBlock {
     fn placed(&self, args: PlacedArgs<'_>) {
         let entity = TestBlockBlockEntity::new(*args.position);
         args.world.add_block_entity(Arc::new(entity));
+    }
+
+    fn emits_redstone_power(&self, _args: EmitsRedstonePowerArgs<'_>) -> bool {
+        true
+    }
+
+    fn get_weak_redstone_power(&self, args: GetRedstonePowerArgs<'_>) -> u8 {
+        let Some(block_entity) = args.world.get_block_entity(args.position) else {
+            return 0;
+        };
+        let Some(test_block) = block_entity.as_any().downcast_ref::<TestBlockBlockEntity>() else {
+            return 0;
+        };
+
+        if test_block.is_powered() { 15 } else { 0 }
+    }
+
+    fn on_scheduled_tick(&self, args: OnScheduledTickArgs<'_>) {
+        let Some(block_entity) = args.world.get_block_entity(args.position) else {
+            return;
+        };
+        let Some(test_block) = block_entity.as_any().downcast_ref::<TestBlockBlockEntity>() else {
+            return;
+        };
+
+        if test_block.is_powered() {
+            test_block.reset(args.world);
+        }
     }
 }
 
